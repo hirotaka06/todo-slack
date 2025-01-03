1e964807999d5738a089a170af9a2a2344b28d55
hirotaka06 <h.kojima572@gmail.com>
2025-01-03 14:55:12 +0900

    change URL2

--- app/TodoTableServer.tsx
+++ app/TodoTableServer.tsx
@@ -1,16 +1,11 @@
-//"https://nextjs-prisma-phi.vercel.app/api/todos"
-
 import React from "react";
 import TodotableClient from "./TodotableClient";
 import { Todo } from "@/app/types";
 
 async function fetchTodos(): Promise<Todo[]> {
-  const response = await fetch(
-    "https://todo-slack-bpk6tdd6t-hirotaka06s-projects.vercel.app/api/todos",
-    {
-      cache: "no-store",
-    }
-  );
+  const response = await fetch("https://todo-slack.vercel.app/api/todos", {
+    cache: "no-store",
+  });
   if (!response.ok) {
     throw new Error("Failed to fetch todos");
   }
